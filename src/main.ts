79fe4d724560f50ac57feea46289a20b7e9b6f4e
Kristina <140779493+kristina-titin@users.noreply.github.com>
2023-11-20 19:57:26 +0700

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+
+import './style.css'
+import App from './App.vue'
+import Blank from './Blank.vue'
+
+createApp(App).mount('#app')
+createApp(Blank).mount('#blank')
